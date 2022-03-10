d91975dcb3d9299a41588e5d842ae21e617212c3
Speeder07 <speeder.x.one@gmail.com>
2022-03-10 20:04:38 +0100

    Functionality01

--- kalendarz/src/index.js
+++ kalendarz/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
 
 const months = new Array('Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień');
 
@@ -38,12 +38,12 @@ function Year(p) {
 }
 
 function Month(p) {
+  const [fullscrean, setFullscrean] = useState(false);
+
 
   return(
-    <div className='month'>
-      <div className='m_name' >{months[p.month]}</div>
-      <MonthContent year={p.year} month={p.month}/>
-      
+    <div className={(fullscrean)?'month fullscrean':'month'} onClick={()=>{setFullscrean(!fullscrean)}}>
+        <MonthContent year={p.year} month={p.month} fullscrean={fullscrean}/>
     </div>
   )
 }
@@ -57,28 +57,72 @@ function MonthContent(p) {
   let enpty_space = (first_day==0) ? 6 : first_day-1;
   return(
     <div className='m_content'>
-      {Array(enpty_space).fill(1).map((el, i) =>
-        <div/>
-      )}
-      {Array(daysInMonth(p.month+1, p.year)).fill(1).map((el, i) =>
-        <Day id={i}/>
-      )}
-    </div>
+      <div className='m_name' >{months[p.month]}</div>
+      <div className='m_center'>
+        {Array(enpty_space).fill(1).map((el, i) =>
+          <div/>
+        )}
+        {Array(daysInMonth(p.month+1, p.year)).fill(1).map((el, i) =>
+          <Day id={i} fullscrean={p.fullscrean}/>
+        )}
+      </div>
+      </div> 
   )
 }
 
 function Day(p) {
-  return(<div className='day'>
-    {p.id+1}
-    <DayForm/>
+
+  const [targeted, setTarget] = useState(false);
+  let [text, setText] = useState('');
+
+  function OnClick(e, id) {
+    e.stopPropagation();
+    console.log(id);
+  }
+
+  function TargetedClick(params) {
+    setTarget(!targeted);
+  }
+
+  function TestChange(e) {
+    setText(e.target.value);
+  }
+
+  return(<div className={(targeted)?'day targeted':'day'} onClick={(event)=>OnClick(event, p.id)}>
+    {(p.fullscrean)?<DayForm id={p.id} targeted={targeted} setTarget={TargetedClick} text={text} textChange={TestChange}/> :p.id+1}
+    
+    
   </div>)
 }
 
 function DayForm(p) {
   
+  
+
+  
   return(
     <div className='day_form'>
+      {(p.targeted)?
+      (
+        <div className='flex_grow'>
+          <div className='flexC'>
+            {p.id+1}
+            <button onClick={p.setTarget}>-</button>
+            <button>S</button>
+          </div>
+          <textarea value={p.text} onChange={p.textChange} >
+            
+          </textarea>
+        </div>
+      ):
+      (
+        <div>
+          <div>{p.id+1}</div>
+          <button onClick={p.setTarget}>+</button>
+        </div>
+      )
       
+      }
       
     </div>
   )
